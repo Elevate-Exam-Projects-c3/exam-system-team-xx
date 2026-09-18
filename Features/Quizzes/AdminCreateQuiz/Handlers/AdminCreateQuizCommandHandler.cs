bcd18934ef45b96f00d4cba81d6445498a220a56
Mark <markehabtawfiq@gmail.com>
2026-09-18 17:25:53 +0300

    feat: Add AdminCreateQuiz feature.

---
+++ Features/Quizzes/AdminCreateQuiz/Handlers/AdminCreateQuizCommandHandler.cs
@@ -0,0 +1,34 @@
+﻿using exam_system.Domain.Entities.Quizzes;
+using exam_system.Features.Quizzes.AdminCreateQuiz.Commands;
+using exam_system.Persistence.DataAccess;
+using exam_system.Shared;
+using MediatR;
+
+namespace exam_system.Features.Quizzes.AdminCreateQuiz.Handlers;
+
+public class AdminCreateQuizCommandHandler : IRequestHandler<AdminCreateQuizCommand, Result<Unit>>
+{
+    private readonly IGenericRepository<Quiz, Guid> _quizRepo;
+
+    public AdminCreateQuizCommandHandler(IGenericRepository<Quiz, Guid> quizRepo)
+    {
+        _quizRepo = quizRepo;
+    }
+
+    public async Task<Result<Unit>> Handle(AdminCreateQuizCommand request, CancellationToken cancellationToken)
+    {
+        // Add new quiz under a specific diploma
+        var newQuiz = new Quiz()
+        {
+            DiplomaId = request.AdminCreateQuizRequest.DiplomaId,
+            Title = request.AdminCreateQuizRequest.Title,
+            Instructions = request.AdminCreateQuizRequest.Instructions,
+            DurationMinutes = request.AdminCreateQuizRequest.DurationMinutes,
+            PassScore = request.AdminCreateQuizRequest.PassScore,
+            MaxAttempts = request.AdminCreateQuizRequest.MaxAttempts,
+        };
+        _quizRepo.Add(newQuiz);
+
+        return Result.Success(Unit.Value);
+    }
+}
